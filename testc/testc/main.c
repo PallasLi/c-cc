d89f1fe7271ef6ef6ca5788b585e741b56092013
pallasLi <ytli1987@163.com>
2015-04-15 17:01:40 +0800

    c/c++学习

---
+++ testc/testc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testc
+//
+//  Created by lyt1987 on 15/4/15.
+//  Copyright (c) 2015年 lyt1987. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ testcc/testcc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testcc
+//
+//  Created by lyt1987 on 15/4/15.
+//  Copyright (c) 2015年 lyt1987. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
